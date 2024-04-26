374436696e7a207c1784ad40a5526de6e879c551
Nishadh KA <nishadhka@gmail.com>
2024-04-26 11:40:18 +0300

    added the tf for vm make

---
+++ gcp-tf/make-vm-tfr/make-vm.tf
@@ -0,0 +1,36 @@
+provider "google" {
+  credentials = file("key.json")
+  project     = "PROJECT NAME"
+  region      = "europe-west2"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "tfrecords-cgan-store"
+  machine_type = "n2-highmem-2"
+  zone         = "europe-west2-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = <<-EOF
+    nka-cgan:${file("nka-cgan-sshkey.pub")}
+    EOF
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
